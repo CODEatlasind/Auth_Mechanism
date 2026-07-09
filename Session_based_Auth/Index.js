15b44a3f62bbaa6be37c460f30838999b6b5f05f
ShashankShekhar <shashankshekharsn042@gmail.com>
2026-07-10 02:48:07 +0530

    ExpressApplication: Created env file and index.js file

---
+++ Session_based_Auth/Index.js
@@ -0,0 +1,24 @@
+const express = reqire("express");
+const sessions = require("express-session");
+require("dotenv").config();
+
+const app = express();
+app.use(
+  sessions({
+    secret: process.env.Session_Secret, // secret key for cryptograsphically signing the seesion ID cookie
+    resave: true, // Forces saving the session details int he session store(generally taxing)
+    saveUninitialized: true, // Saves session details for non initialised sessions as well. Like visiting website for the first time and not logging in. This is generally taxing as well.
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+      //   httpOnly: true, for security reasons, this is set to true so that the cookie cannot be accessed by any client side script. This is a good practice to prevent XSS attacks. XSS injects malicious client side js for obtaining sensitive information like cookies. This is a good practice to prevent XSS attacks.
+      //   secure: true // Inorder to allow only https access to the cookie, this is set to true. This is a good practice to prevent MITM attacks. MITM(Man-in-the-middle attacks) attacks are when an attacker intercepts the communication between the client and server and can read or modify the data being sent. This is a good practice to prevent MITM attacks.
+    },
+  }), // Main object that determines the session configuration like session timeout, security, etc.
+);
+
+app.use(express.json()); // parses the incoming information to json
+app.use(express.urlencoded({ extended: true })); // parses the incming html form data to js object
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
